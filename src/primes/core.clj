554d429c08822ceaff6909018854abe933af64a7
Jon Distad <jon.distad@gmail.com>
2012-03-31 00:58:21 -0400

    Iterative primes in clojure, much faster

--- src/primes/core.clj
+++ src/primes/core.clj
@@ -1,4 +1,4 @@
-(ns om.primes)
+(ns primes.core)
 
 (def primes
   (iterate (fn [[n primes]]
@@ -19,17 +19,36 @@
     []
     (second (nth primes (- n 2)))))
 
-(defn print-primes [n]
-  (let [ps (primes<=n n)
-        len (count ps)]
+(defn print-large-vec [lvec]
+  (let [len (count lvec)]
     (print "[")
     (if (> len 20)
       (do
-        (doseq [p (subvec ps 0 10)]
+        (doseq [p (subvec lvec 0 10)]
           (print (str p " ")))
         (print "... ")
-        (doseq [p (subvec ps (- len 10) (dec len))]
+        (doseq [p (subvec lvec (- len 10) (dec len))]
           (print (str p " "))))
-      (doseq [p (subvec ps 0 (dec len))]
+      (doseq [p (subvec lvec 0 (dec len))]
         (print (str p " "))))
-    (println (str (nth ps (dec len)) "]"))))
+    (println (str (nth lvec (dec len)) "]"))))
+
+(defn print-primes [n]
+  (print-large-vec (primes<=n n)))
+
+(defn iterative-primes [lim]
+  (if (> lim 1)
+    (loop [primes [2]
+           rem-primes primes
+           n 3]
+      (let [p (first rem-primes)]
+        (cond
+          (> n lim)
+          primes
+          (> p (/ n p))
+          (recur (conj primes n) (conj primes n) (inc n))
+          (zero? (rem n p))
+          (recur primes primes (inc n))
+          :else
+          (recur primes (rest rem-primes) n))))
+    []))
